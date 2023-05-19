b22f119c39b20cac02a6743bd2e9dfa32c5cb672
Edwin <eperry910@yahoo.com>
2023-05-19 10:39:39 -0400

    Challenge 5-19-23

---
+++ Intersection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+public class Intersection
+{
+    public static LinkedListNode<T>? findIntersection<T>(LinkedList<T> A, LinkedList<T> B)
+    {
+        LinkedListNode<T>? pointer = A.First;
+        HashSet<LinkedListNode<T>> values = new HashSet<LinkedListNode<T>>();
+        while (pointer != null)
+        {
+            values.Add(pointer);
+            pointer = pointer.Next;
+        }
+        pointer = B.First;
+        while (pointer != null)
+        {
+            if (values.Contains(pointer))
+            {
+                return pointer;
+            }
+            pointer = pointer.Next;
+        }
+        return null;
+    }
+}
---
+++ LoopDetection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+namespace LoopDetection;
+public class Loop
+{
+    public LinkedListNode<T> detectsLoop<T>(LinkedList<T> values)
+    {
+        HashSet<LinkedListNode<T>> foundValues = new HashSet<LinkedListNode<T>>();
+        LinkedListNode<T> pointer = values.First;
+        while (pointer != null)
+        {
+            if (!foundValues.Contains(pointer))
+            {
+                foundValues.Add(pointer);
+            }
+            else
+            {
+                return pointer;
+            }
+        }
+        return null;
+    }
+}
